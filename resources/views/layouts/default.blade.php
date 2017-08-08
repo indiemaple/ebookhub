1486091563ef96e103b1b886d1057a003daf5db1
chenzhenwen <indiemaple@gmail.com>
2017-08-08 08:12:12 +0000

    add home page

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <title>ebookhub</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a></a>
+        </div>
+        <nav>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a>登录</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+@section('content')
+<div class="jumbotron">
+    <h1>ebookhub</h1>
+    <p>电子书分享</p>
+    <div class="search-wraper" role="search">
+        <div class="form-group"><input class="form-control search clearable" placeholder="搜索"
+                                       autocomplete="off" autofocus="" tabindex="0" autocorrect="off"
+                                       autocapitalize="off" spellcheck="false"> <i class="fa fa-search"></i></div>
+    </div>
+</div>
+@stop
